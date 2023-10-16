3d15eeec7bd6aed6b100b802c3a44a4898c2f810
BZR4 <BZR4@users.noreply.github.com>
2023-10-16 20:00:26 -0300

    main
    - Inclusao FeignClient.

--- microservices/product-service/src/main/java/br/com/fatecararas/ldm/productservice/ProductServiceApplication.java
+++ microservices/product-service/src/main/java/br/com/fatecararas/ldm/productservice/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.fatecararas.ldm.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
